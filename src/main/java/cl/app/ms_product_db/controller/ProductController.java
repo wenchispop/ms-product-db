144c25c0e244656f2e085c62d40b0491a7990c4b
wenchispop <debo.palma@duocuc.cl>
2026-05-09 09:30:25 -0400

    Create ProductService
    Create ProductService

---
+++ src/main/java/cl/app/ms_product_db/controller/ProductController.java
@@ -0,0 +1,23 @@
+package cl.app.ms_product_db.controller;
+
+import cl.app.ms_product_db.model.entities.Product;
+import cl.app.ms_product_db.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/product")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping()
+    public ResponseEntity<List<Product>> findAll(){
+        return ResponseEntity.ok(productService.findAll());
+    }
+}
