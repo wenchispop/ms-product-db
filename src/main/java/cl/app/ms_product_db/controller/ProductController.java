5de58de26b998620220380ae65261bd9a6d96127
wenchispop <debo.palma@duocuc.cl>
2026-05-15 20:47:57 -0400

    Update productControlles

--- src/main/java/cl/app/ms_product_db/controller/ProductController.java
+++ src/main/java/cl/app/ms_product_db/controller/ProductController.java
@@ -3,13 +3,13 @@ package cl.app.ms_product_db.controller;
 import cl.app.ms_product_db.model.dto.ProductDTO;
 import cl.app.ms_product_db.model.entities.Product;
 import cl.app.ms_product_db.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
@@ -19,6 +19,19 @@ public class ProductController {
 
     @GetMapping()
     public ResponseEntity<List<ProductDTO>> findAll(){
-        return ResponseEntity.ok(productService.findAll());
+        log.info("Request findAll()");
+        List<ProductDTO> lista = productService.findAll();
+        log.info("Response findAll(): {}", lista);
+        return ResponseEntity.ok(lista);
+        //return ResponseEntity.ok(productService.findAll());
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProductDTO> crearProducto(@RequestHeader(name = "x-country", required = true) String country,
+                                                    @RequestHeader(name = "x-user", required = true) String user,
+                                                    @RequestBody ProductDTO productDTO){
+        log.info("Crear Producto Headers: x-country {}, x-user: {}", country, user);
+        log.info("Crear Producto Request: {},{}", productDTO.toString(), "OK");
+        return ResponseEntity.ok(new ProductDTO());
     }
 }
